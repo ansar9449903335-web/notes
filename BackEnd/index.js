c4e8af2bcfc9e4d6c5d523c2c07a96e50db0c447
ansar <ansar9449903335@gmail.com>
2025-11-14 10:26:03 +0530

    first commit

---
+++ BackEnd/index.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+const express = require('express')
+const RunServer = require("./Database/connection");
+const cors = require ('cors');
+const notesRoutes = require('./Routes/notesRoutes');
+    
+ const app = express()   
+ const PORT = process.env.PORT
+
+
+ app.use(express.json())
+ app.use(cors())
+    
+     
+    RunServer()
+
+ app.use('/api', notesRoutes)   
+
+    app.listen(PORT,()=>{
+        console.log(`server is running on ${PORT}`)
+    })
+    
